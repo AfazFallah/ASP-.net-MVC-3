9065ce8ed402c6f32318ccb3adc12bc21a7bfb51
Afazfallah <afallah488@gmail.com>
2024-03-15 23:53:18 +0330

    feet-config project file

--- ASP .net MVC 3/Controllers/StudentsController.cs
+++ ASP .net MVC 3/Controllers/StudentsController.cs
@@ -1,9 +1,11 @@
 ﻿using ASP.net_MVC_3.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
 
 namespace ASP.net_MVC_3.Controllers
 {
@@ -21,10 +23,31 @@ namespace ASP.net_MVC_3.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Create([Bind(Include = "Name , Family , NationalCode , Age , Phone , Password , Email , Gender , Image")]T_Students students)
+        public ActionResult Create([Bind(Include = "Name , Family , NationalCode , Age , Phone , Password , Email , Gender , Image")]T_Students students , HttpPostedFileBase Image)
         {
          if (ModelState.IsValid)
             {
+                string newImage = "user.jpg";
+
+                if (Image!=null)
+                {
+                    if (Image.ContentType != "image/jpeg" && Image.ContentType != "image/png" && Image.ContentType != "image/jpg")
+                    {
+                        ModelState.AddModelError("Image", "تصویر شما باید با فرمت jpeg  یا png یا jpg باشد!");
+                        return View();
+                    }
+                    if (Image.ContentLength > 300000)
+                    {
+                        ModelState.AddModelError("Image", "حجم تصویر شما نباید بیشتر از 300 کیلوبایت باشد!");
+                        return View();
+                    }
+                    newImage = Guid.NewGuid().ToString().Replace("-","")+Path.GetExtension(Image.FileName);
+                    Image.SaveAs(Server.MapPath("/Image/ProfileName/" + newImage));
+                }
+
+
+
+                students.Image = newImage;
                 students.IsActive = true;
                 students.RegisterDate = DateTime.Now;
                 db.T_Students.Add(students);
@@ -32,6 +55,21 @@ namespace ASP.net_MVC_3.Controllers
                 return Redirect("Index");
             }
          else { return View(); }
+        }
+        [HttpGet]
+        public ActionResult Details(int? id) 
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var student = db.T_Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
         }
 
     }
--- ASP .net MVC 3/Models/T_Students.cs
+++ ASP .net MVC 3/Models/T_Students.cs
@@ -7,54 +7,70 @@ namespace ASP.net_MVC_3.Models
 
     public partial class T_Students
     {
+        #region Anno
         [Display(Name = "آیدی")]
-        [Required(ErrorMessage = " فیلد {0} نمیتواند خالی باشد")]
+        [Required(ErrorMessage = " فیلد {0} نمیتواند خالی باشد")] 
+        #endregion
         public int Id { get; set; }
-
+        #region Anno
         [Display(Name = "نام")]
         [Required(ErrorMessage = " فیلد {0} نمیتواند خالی باشد")]
-        [StringLength(20, MinimumLength = 2, ErrorMessage = "لطفا {0} را به صورت صحیح وارد کنید (حداقل 2 و حداکثر 20 کاراکتر)")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "لطفا {0} را به صورت صحیح وارد کنید (حداقل 2 و حداکثر 20 کاراکتر)")] 
+        #endregion
         public string Name { get; set; }
-
+        #region Anno
         [Display(Name = "نام خانوادگی")]
         [Required(ErrorMessage = " فیلد {0} نمیتواند خالی باشد")]
-        [StringLength(20, MinimumLength = 2, ErrorMessage = "لطفا {0} را به صورت صحیح وارد کنید (حداقل 2 و حداکثر 20 کاراکتر)")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "لطفا {0} را به صورت صحیح وارد کنید (حداقل 2 و حداکثر 20 کاراکتر)")] 
+        #endregion
         public string Family { get; set; }
-
+        #region Anno
         [Display(Name = "سن")]
-        [Required(ErrorMessage = " فیلد {0} نمیتواند خالی باشد")]
+        [Required(ErrorMessage = " فیلد {0} نمیتواند خالی باشد")] 
+        #endregion
         public int Age { get; set; }
-
+        #region Anno
         [Display(Name = "موبایل")]
         [Required(ErrorMessage = " فیلد {0} نمیتواند خالی باشد")]
-        [RegularExpression("(09)[0-9]{9}", ErrorMessage = "لطفا {0} را به صورت صحیح وارد کنید")]
+        [RegularExpression("(09)[0-9]{9}", ErrorMessage = "لطفا {0} را به صورت صحیح وارد کنید")] 
+        #endregion
         public string Phone { get; set; }
-
+        #region Anno
         [Display(Name = "ایمیل")]
-        [EmailAddress(ErrorMessage = "لطفا {0} را به صورت صحیح وارد کنید")]
+        [EmailAddress(ErrorMessage = "لطفا {0} را به صورت صحیح وارد کنید")] 
+        #endregion
         public string Email { get; set; }
-
+        #region Anno
         [Display(Name = "تاریخ عضویت")]
-        [Required(ErrorMessage = " فیلد {0} نمیتواند خالی باشد")]
+        [Required(ErrorMessage = " فیلد {0} نمیتواند خالی باشد")] 
+        #endregion
         public System.DateTime RegisterDate { get; set; }
+        #region Anno
 
         [Display(Name = "وضعیت")]
-        [Required(ErrorMessage = " فیلد {0} نمیتواند خالی باشد")]
+        [Required(ErrorMessage = " فیلد {0} نمیتواند خالی باشد")] 
+        #endregion
         public bool IsActive { get; set; }
-
+        #region Anno
         [Display(Name = "جنسیت")]
+        [Required(ErrorMessage = " فیلد {0} نمیتواند خالی باشد")] 
+        #endregion
         public Nullable<bool> Gender { get; set; }
-
-        [Display(Name = "عکس")]
+        #region Anno
+        [Display(Name = "تصویر")] 
+        #endregion
         public string Image { get; set; }
-
+        #region Anno
         [Display(Name = "رمز ورود")]
         [Required(ErrorMessage = " فیلد {0} نمیتواند خالی باشد")]
         [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "لطفا {0} را به صورت صحیح وارد کنید (حداقل 8 و حداکثر 20 کاراکتر)")] 
+        #endregion
         public string Password { get; set; }
-
+        #region Anno
         [Display(Name = "کد ملی")]
-        [RegularExpression("[0-9]{10}", ErrorMessage = "لطفا {0} را به صورت صحیح وارد کنید")]
+        [RegularExpression("[0-9]{10}", ErrorMessage = "لطفا {0} را به صورت صحیح وارد کنید")] 
+        #endregion
         public string NationalCode { get; set; }
     }
 }
